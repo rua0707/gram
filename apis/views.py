e8545763771921b2b0719c27954a8b2c6cda95a9
Seungkyoum Kim <islwo123i@gmail.com>
2020-04-27 15:07:47 +0900

    create login

---
+++ apis/views.py
@@ -0,0 +1,60 @@
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.core.validators import validate_email, ValidationError
+from django.contrib.auth import authenticate, login
+
+class BaseView(View):
+    @staticmethod
+    def response(data={}, message='', status=200):
+        result = {
+            'data': data,
+            'message': message,
+        }
+        return JsonResponse(result, status)
+
+
+class UserCreateView(BaseView):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='아이디를 입력해주세요.', status=400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='패스워드를 입력해주세요.', status=400)
+        email = request.POST.get('email', '')
+        try:
+            validate_email(email)
+        except ValidationError:
+            self.response(message='올바른 이메일을 입력해주세요.', status=400)
+
+        try:
+            user = User.objects.create_user(username, email, password)
+        except IntegrityError:
+            return self.response(message='이미 존재하는 아이디입니다.', status=400)
+
+        return self.response({'user.id': user.id})
+
+
+class UserLoginView(BaseView):
+    def post(self, request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='아이디를 입력해주세요', status=400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='비밀번호를 입력해주세요', status=400)
+
+        user = authenticate(request, username=username, password=password) # authenticate 는 아이디나 패스워드를 잘못입력하면 None를 반환해준다.
+        if user in None:
+            return self.response(message='입력 정보를 확인해주세요.', status=400)
+        login(request, user)
+
+        return self.response
--- gram/urls.py
+++ gram/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from contents.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('apis/', include('apis.urls')),
+    path('', HomeView.as_view(), name='contents_home'),
 ]
