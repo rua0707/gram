eef13a68135aeae7aab165dcdc5c4cccaac1a19f
Seungkyoum Kim <islwo123i@gmail.com>
2020-06-10 18:53:42 +0900

    up

--- gram/urls.py
+++ gram/urls.py
@@ -15,11 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
-from contents.views import HomeView
+from contents.views import HomeView, RelationView
+
+class NonUserTemplateView(TemplateView):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_anonymous:
+            return redirect('contents_home')
+        return  super(NonUserTemplateView, self).dispatch(request, *args, **kwargs)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('apis/', include('apis.urls')),
     path('', HomeView.as_view(), name='contents_home'),
+    path('login/', NonUserTemplateView.as_view(template_name='login.html'), name='login'),
+    path('register/', NonUserTemplateView.as_view(template_name='register.html'), name='register'),
+    path('relation/', RelationView.as_view(), name='contents_relation'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    #urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
